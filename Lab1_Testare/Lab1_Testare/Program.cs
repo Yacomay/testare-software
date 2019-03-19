e2b47f61b85454d92ebfc336981b0711beb0e4bc
Corneliu Iarincovschi <cornel.iarincovschi@gmail.com>
2019-03-19 13:21:37 +0200

    Finished Lab 1

---
+++ Lab1_Testare/Lab1_Testare/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Lab1_Testare
+{
+    class Program
+    {
+        static void Main()
+        {
+            Test1 test = new Test1();
+            test.StartBrowser();
+            test.Test();
+            test.CloseBrowser();
+        }
+    }
+}
---
+++ Lab1_Testare/Lab1_Testare/Test1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Lab1_Testare
+{
+    class Test1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\Cornel\\Desktop\\Chrome Driver");
+        }
+
+        [Test]
+        public void Test()
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+                driver.Url = "file:///C:/Uni/Repository/testare-software/Lab1_Testare/Lab1_Testare/Webpage.html";
+                driver.FindElement(By.Id("loginName")).SendKeys("test");
+                driver.FindElement(By.Id("loginPass")).SendKeys("run");
+                driver.FindElement(By.Id("btnLogin")).Click();
+                wait.Until(wt => wt.FindElement(By.Id("secret")));
+                Console.WriteLine("Login Succesful");
+                Console.ReadKey();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error message: ", exc.Message);
+                return;
+            }
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
